9b47274ee48cda2a190dc2e89aa7a01d19d28bb0
RizaP <rizalevi2705@gmail.com>
2026-08-08 18:44:13 +0700

    fix: add path alias and resolve folder casing for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,24 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'node:url'; // <--- TAMBAHKAN INI
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx', // Cukup app.jsx saja
+            input: 'resources/js/app.jsx',
             refresh: true,
             buildDirectory: 'build',
         }),
         react(),
         tailwindcss(),
     ],
+    // TAMBAHKAN BAGIAN ALIAS RESOLVE DI BAWAH INI
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+        },
+    },
     build: {
         cssMinify: 'esbuild',
     },
