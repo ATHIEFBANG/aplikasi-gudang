e383b6c33e78b0eef369d46d339edca4d9f3e066
RizaP <rizalevi2705@gmail.com>
2026-08-08 21:14:56 +0700

    fix: add trustProxies configuration for vercel pagination

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Percayai Reverse Proxy dari Vercel
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
