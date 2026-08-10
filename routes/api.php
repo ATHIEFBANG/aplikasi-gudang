f2020a87d2b8e62abd97749efc09d60b62acb9f0
RizaP <rizalevi2705@gmail.com>
2026-08-10 21:59:06 +0700

    fix: update vercel region to Singapore and trustProxies

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DataManagementController; // 🟢 Import Controller
+//use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// 🟢 Route Endpoint Sync Bulk RPM dari Google Apps Script
-Route::post('/rpm/sync-bulk', [DataManagementController::class, 'syncBulkRpm']);
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
